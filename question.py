39e147b09931d8ddb47bad675a87bf4eed59b1e5
Адель Хакимов <harso96@mail.ru>
2024-10-04 14:59:16 +0300

    fix/get random yes, no and sounds

--- question.py
+++ question.py
@@ -33,18 +33,18 @@ def get_answer(session, version, command, song):
 
 
 def correct_answer(session, version):
-    text_message = TextMessages.correct_answer
+    text_message = TextMessages.correct_answer.format(get_random_element(YES))
     #audio_message = f'<speaker audio="alice-sounds-game-powerup-1.opus">{AudioMessages.correct_answer_sound}'
-    audio_message = f'{get_random_element(WIN_SOUND_URLS)}{AudioMessages.correct_answer_sound}'
+    audio_message = f'{get_random_element(WIN_SOUND_URLS)}{AudioMessages.correct_answer_sound.format(get_random_element(YES))}'
     buttons = Buttons.menu_buttons
 
     return returned_data(session, version, text_message, audio_message, buttons)
 
 
 def incorrect_answer(session, version):
-    text_message = TextMessages.incorrect_answer
+    text_message = TextMessages.incorrect_answer.format(get_random_element(NO))
     #audio_message = f'<speaker audio="alice-sounds-game-loss-1.opus">{AudioMessages.incorrect_answer_sound}'
-    audio_message = f'<speaker audio="alice-sounds-game-loss-1.opus">{AudioMessages.incorrect_answer_sound}'
+    audio_message = f'{get_random_element(LOSE_SOUND_URLS)}{AudioMessages.incorrect_answer_sound.format(get_random_element(NO))}'
     buttons = Buttons.menu_buttons
 
     return returned_data(session, version, text_message, audio_message, buttons)
