994a21ba26a68e73a5a24b7855c33a23295f6bfb
Адель Хакимов <harso96@mail.ru>
2024-10-29 14:30:12 +0300

    feat/add OOP

--- commands.py
+++ commands.py
@@ -1,70 +1,83 @@
+from constants import (
+    TextMessages, AudioMessages, Buttons, 
+    YES, NO,
+    WIN_SOUND_URLS, LOSE_SOUND_URLS
+)
+from data import music
+from utils import get_random_element, get_song, returned_data
+
+
 class Command:
 
-    def __init__(self, text_message, audio_message, buttons, returned_data):
-        self.text_message = text_message
-        self.audio_message = audio_message
-        self.buttons = buttons
-        self.returned_data = returned_data
+    def __init__(self, session, version):
+        self.session = session
+        self.version = version
 
-    
-    def first_question(session, version, command, song):
+
+    def welcome(self):
+        text_message = TextMessages.welcome_message
+        audio_message = f'<speaker audio="alice-sounds-game-powerup-1.opus">{AudioMessages.welcome_sound}'
+        buttons = Buttons.start_button
+        return returned_data(self.session, self.version, text_message, audio_message, buttons)
+
+
+    def first_question(self, song):
         sound = song.get('sound')
         text_message = TextMessages.first_question.format(len(music))
         audio_message = AudioMessages.sound_first_question.format(len(music), sound)
         buttons = Buttons.menu_buttons
-        return returned_data(session, version, text_message, audio_message, buttons)
+        return returned_data(self.session, self.version, text_message, audio_message, buttons)
 
 
-    def correct_answer(session, version):
+    def correct_answer(self):
         text_message = TextMessages.correct_answer.format(get_random_element(YES))
-        #audio_message = f'<speaker audio="alice-sounds-game-powerup-1.opus">{AudioMessages.correct_answer_sound}'
         audio_message = f'{get_random_element(WIN_SOUND_URLS)}{AudioMessages.correct_answer_sound.format(get_random_element(YES))}'
         buttons = Buttons.menu_buttons
         song = get_song(music)
-        return returned_data(session, version, text_message, audio_message, buttons, song)
+        return returned_data(self.session, self.version, text_message, audio_message, buttons, song)
 
 
-    def incorrect_answer(session, version):
+    def incorrect_answer(self):
         text_message = TextMessages.incorrect_answer.format(get_random_element(NO))
-        #audio_message = f'<speaker audio="alice-sounds-game-loss-1.opus">{AudioMessages.incorrect_answer_sound}'
         audio_message = f'{get_random_element(LOSE_SOUND_URLS)}{AudioMessages.incorrect_answer_sound.format(get_random_element(NO))}'
         buttons = Buttons.menu_buttons
-        return returned_data(session, version, text_message, audio_message, buttons)
+        return returned_data(self.session, self.version, text_message, audio_message, buttons)
 
 
-    def help_command(session, version):
+    def help_command(self):
         text_message = TextMessages.help_text
         audio_message = AudioMessages.help_sound
         buttons = Buttons.menu_buttons
-        return returned_data(session, version, text_message, audio_message, buttons)
+        return returned_data(self.session, self.version, text_message, audio_message, buttons)
 
 
-    def repeate_command(session, version):
-        text_mesage = TextMessages.repeate_text
+    def repeat_command(self):
+        text_message = TextMessages.repeate_text
         audio_message = AudioMessages.repeate_sound
         buttons = Buttons.menu_buttons
-        return returned_data(session, version, text_mesage, audio_message, buttons)
+        return returned_data(self.session, self.version, text_message, audio_message, buttons)
 
 
-    def capitulate_command(session, version):
+    def capitulate_command(self):
         text_message = TextMessages.capitulate_text
         audio_message = AudioMessages.capitulate_sound
         buttons = Buttons.menu_buttons
-        return returned_data(session, version, text_message, audio_message, buttons)
+        return returned_data(self.session, self.version, text_message, audio_message, buttons)
 
 
-    def exit_command(session, version):
+    def exit_command(self):
         text_message = TextMessages.exit_text
         audio_message = AudioMessages.exit_sound
         buttons = Buttons.menu_buttons
-        return returned_data(session, version, text_message, audio_message, buttons)
+        return returned_data(self.session, self.version, text_message, audio_message, buttons)
 
 
-    def get_answer(session, version, command, song):
+    def get_answer(self, command, song):
         if command in song.get('answer'):
-            return correct_answer(session, version)
+            return self.correct_answer()
         else:
-            return incorrect_answer(session, version)
+            return self.incorrect_answer()
 
 
-first_question = Command(TextMessages.first_question, AudioMessages.sound_first_question, Buttons.menu_buttons, first_question)
+command_handler = Command(session, version)
+welcome = command_handler.welcome()
